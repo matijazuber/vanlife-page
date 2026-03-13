86f5320319259261abca51f6e6c466773f87148a
Matija Zuber <matijazuber273@gmail.com>
2026-03-13 15:44:04 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import About from "./about";
+import Home from "./home";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <div className="headerDiv">
+          <Link
+            style={{
+              lineHeight: "40px",
+              fontSize: "25px",
+              verticalAlign: "middle",
+              fontFamily: "Inter",
+              fontWeight: "900",
+              color: "black",
+              textDecoration: "none",
+            }}
+            to="/"
+          >
+            #VANLIFE
+          </Link>
+          <div
+            style={{
+              display: "flex",
+              fontFamily: "Inter",
+              fontWeight: "600",
+              lineHeight: "23px",
+              gap: "30px",
+            }}
+          >
+            <Link
+              style={{ color: "#4D4D4D", textDecoration: "none" }}
+              to="/about"
+            >
+              About
+            </Link>
+            <Link
+              style={{ color: "#4D4D4D", textDecoration: "none" }}
+              to="/vans"
+            >
+              Vans
+            </Link>
+          </div>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/home.jsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <>
+      <main>
+        <p>You got the travel plans,we got the travel vans</p>
+        <p className="desc">
+          Add adventure to your life by joining the #vanlife movement. Rent the
+          perfect van to make your perfect road trip.
+        </p>
+        <button className="button">Find your van</button>
+      </main>
+      <footer>
+        <p>Ⓒ 2022 #VANLIFE</p>
+      </footer>
+    </>
+  );
+}
