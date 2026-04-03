2693c435c27c0698fe9a663505612190064bd07e
Matija Zuber <matijazuber273@gmail.com>
2026-04-03 14:08:46 +0200

    styled and made the vanInfo page

--- src/pages/vanInfo.jsx
+++ src/pages/vanInfo.jsx
@@ -1,5 +1,37 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
 const VanInfo = () => {
-  return <h1>Info about the Vans goes here!</h1>;
+  const params = useParams();
+  const [van, setVan] = React.useState();
+
+  React.useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => setVan(data.vans));
+  }, [params.id]);
+
+  return (
+    <>
+      {van ? (
+        <div key={van.id} className="van-tile-info">
+          <img src={van.imageUrl}></img>
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+          <div className="van-info">
+            <h3>{van.name}</h3>
+            <p>
+              ${van.price}
+              <span>/day</span>
+            </p>
+            <i className="van-description">{van.description}</i>
+            <button className="link-button">Rent this van</button>
+          </div>
+        </div>
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </>
+  );
 };
 
 export default VanInfo;
