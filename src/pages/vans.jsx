7118931a1a173d6bc72616efa23446d3cc2c308c
Matija Zuber <matijazuber273@gmail.com>
2026-04-03 09:49:43 +0200

    added in the vans compartment, mirage api call so it renders the vans using fetch and useEffect

--- src/pages/vans.jsx
+++ src/pages/vans.jsx
@@ -1,5 +1,36 @@
+import React from "react";
+
 const Vans = () => {
-  return <h1>Vans page goes here 🚐</h1>;
+  const [vansData, setVansData] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("/api/vans")
+      .then((res) => res.json())
+      .then((data) => setVansData(data.vans));
+  }, []);
+
+  console.log(vansData);
+
+  const vanElements = vansData.map((van) => (
+    <div key={van.id}>
+      <img src={van.imageUrl}></img>
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <p>
+          ${van.price}
+          <span>/day</span>
+        </p>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+      </div>
+    </div>
+  ));
+
+  return (
+    <>
+      <h1>Explore our Van options</h1>
+      <div>{vanElements}</div>
+    </>
+  );
 };
 
 export default Vans;
