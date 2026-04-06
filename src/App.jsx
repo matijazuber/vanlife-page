dd7bcf6e53011ec97b712ba0a591c3d9a0eb3055
Matija Zuber <matijazuber273@gmail.com>
2026-04-06 10:07:28 +0200

    Added the new Host link with 3 compnents(dashboard,income,reviews), also moved the Header to it's own compnent and linked it using nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,50 +4,25 @@ import Home from "./pages/home";
 import Footer from "./footer";
 import Vans from "./pages/vans";
 import VanInfo from "./pages/vanInfo";
-
+import Layout from "./components/Layout";
 import "./server/server";
+import Dashboard from "./components/host/Dashboard";
+import Income from "./components/host/Income";
+import Reviews from "./components/host/Reviews";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="headerDiv">
-        <Link
-          style={{
-            lineHeight: "40px",
-            fontSize: "25px",
-            verticalAlign: "middle",
-            fontFamily: "Inter",
-            fontWeight: "900",
-            color: "black",
-            textDecoration: "none",
-          }}
-          to="/"
-        >
-          #VANLIFE
-        </Link>
-        <div
-          style={{
-            display: "flex",
-            fontFamily: "Inter",
-            fontWeight: "600",
-            lineHeight: "23px",
-            gap: "30px",
-          }}
-        >
-          <Link className="about-header" to="/about">
-            About
-          </Link>
-          <Link className="vans-header" to="/vans">
-            Vans
-          </Link>
-        </div>
-      </div>
-
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanInfo />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanInfo />} />
+          <Route path="/host" element={<Dashboard />} />
+          <Route path="/host/income" element={<Income />} />
+          <Route path="/host/reviews" element={<Reviews />} />
+        </Route>
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <>
+      <header className="headerDiv">
+        <Link
+          style={{
+            lineHeight: "40px",
+            fontSize: "25px",
+            verticalAlign: "middle",
+            fontFamily: "Inter",
+            fontWeight: "900",
+            color: "black",
+            textDecoration: "none",
+          }}
+          to="/"
+        >
+          #VANLIFE
+        </Link>
+        <div
+          style={{
+            display: "flex",
+            fontFamily: "Inter",
+            fontWeight: "600",
+            lineHeight: "23px",
+            gap: "30px",
+          }}
+        >
+          <Link className="host-header" to="/host">
+            Host
+          </Link>
+          <Link className="about-header" to="/about">
+            About
+          </Link>
+          <Link className="vans-header" to="/vans">
+            Vans
+          </Link>
+        </div>
+      </header>
+      <Outlet></Outlet>
+    </>
+  );
+};
+
+export default Header;
