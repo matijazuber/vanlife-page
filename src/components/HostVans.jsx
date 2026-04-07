25f012975dfd80d8b303c67f7a40725ced34f89f
Matija Zuber <matijazuber273@gmail.com>
2026-04-07 14:50:19 +0200

    created the listed vans component

--- src/components/HostVans.jsx
+++ src/components/HostVans.jsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+
 const HostVans = () => {
-  return <h1>My vans go here</h1>;
+  const [hostVans, setHostVans] = useState([]);
+
+  useEffect(() => {
+    const fetchVans = async () => {
+      const res = await fetch("/api/host/vans");
+      const data = await res.json();
+      console.log(data.vans);
+
+      setHostVans(data.vans);
+    };
+
+    fetchVans();
+  }, []);
+
+  const hostVanElements = hostVans.map((van) => {
+    return (
+      <>
+        <div className="hostVanDiv" key={van.id}>
+          <div>
+            <img src={van.imageUrl}></img>
+            <div className="vanPriceName">
+              <p>{van.name}</p>
+              <span>{`$${van.price}/day`}</span>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  });
+  return (
+    <>
+      <h1 style={{ fontFamily: "Inter", marginLeft: "25px" }}>
+        Your listed vans
+      </h1>
+      {hostVanElements}
+    </>
+  );
 };
 
 export default HostVans;
