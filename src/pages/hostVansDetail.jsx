0ae3acda3a06ffe859fbc313a4d1933119974ba9
Matija Zuber <matijazuber273@gmail.com>
2026-04-08 10:33:31 +0200

    put params in the host vans detail

--- src/pages/hostVansDetail.jsx
+++ src/pages/hostVansDetail.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 const MyVansDetail = () => {
+  const params = useParams();
+  const [van, setVan] = useState();
+
+  useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => setVan(data.vans));
+  }, [params.id]);
+
   return (
     <>
-      <div>
-        <div></div>
-      </div>
+      {van ? (
+        <div>
+          <div>
+            <img src={van.imageUrl}></img>
+          </div>
+        </div>
+      ) : null}
     </>
   );
 };
